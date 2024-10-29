bdadb82ad429ee875391ceee1964d61e9c930c7e
hsedjame <sedhjodev@gmail.com>
2024-10-29 14:42:38 +0100

    [react-redux] introduce reselect

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,9 +1,16 @@
 import {AppState} from "./state.ts";
+import {createSelector} from "reselect";
 
-export const selectTodoLabels = (state: AppState) => state.todos.map(todo => todo.label)
+export const selectTodos = (state: AppState) => state.todos
 
 export const selectSearchTerm = (state: AppState) => state.searchTerm
 
-export const selectFilteredTodos = (state: AppState) => {
+/*export const selectFilteredTodos = (state: AppState) => {
     return state.todos.filter(todo => todo.label.toLowerCase().includes(state.searchTerm.toLowerCase()))
-}
+}*/
+
+export const selectFilteredTodos = createSelector(
+    selectTodos,
+    selectSearchTerm,
+    (todos, searchTerm) =>  todos.filter(todo => todo.label.toLowerCase().includes(searchTerm.toLowerCase()))
+)
